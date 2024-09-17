0dcd607bcd2aa37cd47321e9719d1e0c772e67b4
Atakhan <example@gmail.com>
2024-09-17 19:52:12 +0600

    lesson Objects and their methods

--- script.js
+++ script.js
@@ -1,57 +1,34 @@
 "use strict";
-let numberOfFilms;
 
-function start() {
-  numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
-
-  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
-    numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
-  }
-}
-// start();
-
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
+const options = {
+  name: "test",
+  width: 1024,
+  height: 1024,
+  color: {
+    border: "black",
+    bg: "red",
+  },
+  makeTest: function () {
+    console.log("Test");
+  },
 };
 
-function rememberMyFilms() {
-  for (let i = 0; i < 2; i++) {
-    const a = prompt("Один из последних просмотренных фильмов?", "").trim(),
-      b = prompt("На сколько оцените его?", "");
-    if (a != null && b != null && (a != "") & (b != "") && a.length < 50) {
-      personalMovieDB.movies[a] = b;
-    }
-  }
-}
-// rememberMyFilms();
+options.makeTest();
+const { border, bg } = options.color;
+console.log(border);
+// console.log(Object.keys(options).length);
 
-function detectPersonalLevel() {
-  if (personalMovieDB.count < 10) {
-    console.log("Просмотрено довольно мало фильмов");
-  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Вы классический зритель");
-  } else if (personalMovieDB.count >= 30) {
-    console.log("Вы киноман");
-  } else {
-    console.log("Произошла ошибка!");
-  }
-}
-// detectPersonalLevel();
+// let counter = 0;
 
-function showMyDB(hidden) {
-  if (!hidden) {
-    console.log(personalMovieDB);
-  }
-}
-// showMyDB(personalMovieDB.privat);
-
-function writeYourGenres() {
-  for (let i = 1; i <= 3; i++) {
-    personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-  }
-}
-// writeYourGenres();
+// for (let key in options) {
+//   if (typeof options[key] === "object") {
+//     for (let i in options[key]) {
+//       console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//       counter++;
+//     }
+//   } else {
+//     console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//     counter++;
+//   }
+// }
+// console.log(counter);
