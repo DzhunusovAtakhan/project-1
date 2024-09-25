c07838e9671aed05651ddb9d48b63be974850c42
Atakhan <example@gmail.com>
2024-09-25 14:58:48 +0600

    Таймер обратного счета

--- script.js
+++ script.js
@@ -1,25 +1,52 @@
 "use strict";
 
-const now = new Date();
+const deadLine = "2024-09-26";
 
-console.log(now.setHours(18));
-console.log(now);
+function getTimeDiff(endtime) {
+  const t = Date.parse(endtime) - Date.parse(new Date()),
+    days = Math.floor(t / (1000 * 60 * 60 * 24)),
+    hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+    minutes = Math.floor((t / (1000 * 60)) % 60),
+    seconds = Math.floor((t / 1000) % 60);
 
-// console.log(now.getFullYear());
-// console.log(now.getMonth());
-// console.log(now.getDate());
-// console.log(now.getDay());
-// console.log(now.getHours());
-// console.log(now.getUTCHours());
-
-// console.log(now.getTimezoneOffset());
-// console.log(now.getTime());
-
-let start = new Date();
-
-for (let i = 0; i < 100000; i++) {
-  let some = i ** 3;
+  return {
+    total: t,
+    days: days,
+    hours: hours,
+    minutes: minutes,
+    seconds: seconds,
+  };
 }
 
-let end = new Date();
-alert(`Цикл отработал за ${end - start} миллисекунд`);
+function getZero(num) {
+  if (num < 10 || num <= 0) {
+    return `0${num}`;
+  } else {
+    return num;
+  }
+}
+
+function setClock(selector, endtime) {
+  const timer = document.querySelector(selector),
+    days = timer.querySelector("#days"),
+    hours = timer.querySelector("#hours"),
+    minutes = timer.querySelector("#minutes"),
+    seconds = timer.querySelector("#seconds"),
+    timeInterval = setInterval(updateClock, 1000);
+
+  updateClock();
+
+  function updateClock() {
+    const t = getTimeDiff(endtime);
+
+    days.innerHTML = getZero(t.days);
+    hours.innerHTML = getZero(t.hours);
+    minutes.innerHTML = getZero(t.minutes);
+    seconds.innerHTML = getZero(t.seconds);
+
+    if (t.total <= 0) {
+      clearInterval(timeInterval);
+    }
+  }
+}
+setClock(".timer", deadLine);
