b8400aaf4e82e05ddbe69d319f9e75f0555943df
Atakhan <example@gmail.com>
2024-09-25 19:36:35 +0600

    Контекст вызова. This

--- script.js
+++ script.js
@@ -1,24 +1,71 @@
 "use strict";
 
+function showThis(a, b) {
+  console.log(this);
+  function sum() {
+    console.log(this);
+    return a + b;
+  }
+  console.log(sum());
+}
+// showThis(4, 5);
+
+const obj = {
+  a: 20,
+  b: 15,
+  sum: function () {
+    console.log(this);
+  },
+};
+// obj.sum();
+
 function User(name, id) {
   this.name = name;
   this.id = id;
   this.human = true;
-  this.hello = () => {
+  this.hello = function () {
     console.log(`Hello ${this.name}`);
   };
 }
+const ivan = new User("Ivan", 28);
 
-User.prototype.exit = function () {
-  console.log(`Пользователь ${this.name} ушел`);
+function sayName(surename) {
+  console.log(this);
+  console.log(this.name + " " + surename);
+}
+
+const user = {
+  name: "John",
 };
 
-const ivan = new User("Ivan", 28);
-const alex = new User("Alex", 20);
+// sayName.call(user, "Smith");
+// sayName.apply(user, ["Smith"]);
 
-ivan.exit();
+function count(num) {
+  return this * num;
+}
+const double = count.bind(2);
+// console.log(double(4));
 
-ivan.hello();
-alex.hello();
+//1 Обычная функия this - window, но если use strict - undefined
+//2 Контекст у методов обьекта - сам обьект
+//3 this в конструкторах и классах - это новый экземпляр обьекта
+//4 Ручная привязка this: call, apply, bind
 
-console.log(ivan, alex);
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", function () {
+  this.style.backgroundColor = "red";
+});
+
+const obj1 = {
+  num: 5,
+  sayNumber: function () {
+    const say = () => {
+      console.log(this);
+    };
+    say();
+  },
+};
+
+obj1.sayNumber();
