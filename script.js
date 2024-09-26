a9134cf5d2f0370d829604ac46b45f8bb8913f24
Atakhan <example@gmail.com>
2024-09-26 15:33:44 +0600

    rest оператор и параметры по умолчанию

--- script.js
+++ script.js
@@ -1,32 +1,23 @@
 "use strict";
 
-class Rectangle {
-  constructor(height, width) {
-    this.height = height;
-    this.width = width;
+const log = function (a, b, ...rest) {
+  console.log(a, b, ...rest);
+};
+
+// log(400, 500, "operator", "usage");
+
+function calcOrDouble(number, basis = 2) {
+  console.log(number * basis);
+}
+// calcOrDouble(3);
+
+function getSum(a, b) {
+  function sum() {
+    console.log(this.a);
+    return a + b;
   }
 
-  calcArea() {
-    return this.height * this.width;
-  }
+  console.log(sum());
 }
 
-class ColoredRectangleWithText extends Rectangle {
-  constructor(height, width, text, bgColor) {
-    super(height, width);
-    this.text = text;
-    this.bgColor = bgColor;
-  }
-
-  showMyProps() {
-    console.log(`text: ${this.text}; BGColor: ${this.bgColor}`);
-  }
-}
-
-const someDiv = new ColoredRectangleWithText(25, 10, "Hello", "blue");
-
-someDiv.showMyProps();
-console.log(someDiv.calcArea());
-
-// const square = new Rectangle(10, 10);
-// console.log(square.calcArea());
+getSum(4, 5);
