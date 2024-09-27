694bd8c65aee67bf41aed59d1cb409c4b090c14b
Atakhan <example@gmail.com>
2024-09-27 12:53:57 +0600

    Задачи на методы перебора массива

--- script.js
+++ script.js
@@ -25,14 +25,91 @@
 // const res = arr.reduce((sum, current) => `${sum}, ${current}`);
 // console.log(res);
 
-const obj = {
-  taha: "person",
-  ann: "person",
-  dog: "animal",
-  cat: "animal",
-};
+// const obj = {
+//   taha: "person",
+//   ann: "person",
+//   dog: "animal",
+//   cat: "animal",
+// };
 
-const newArr = Object.entries(obj)
-  .filter((item) => item[1] == "person")
-  .map((item) => item[0]);
-console.log(newArr);
+// const newArr = Object.entries(obj)
+//   .filter((item) => item[1] == "person")
+//   .map((item) => item[0]);
+// console.log(newArr);
+
+//1.1Task
+const films = [
+  {
+    name: "Titanic",
+    rating: 9,
+  },
+  {
+    name: "Die hard 5",
+    rating: 5,
+  },
+  {
+    name: "Matrix",
+    rating: 8,
+  },
+  {
+    name: "Some bad film",
+    rating: 4,
+  },
+];
+
+function showGoodFilms(arr) {
+  return arr.filter((item) => item.rating >= 8);
+}
+// console.log(showGoodFilms(films));
+
+//1.2Task
+function showListOfFilms(arr) {
+  return arr.map((obj) => `${obj.name}`).join(", ");
+}
+// console.log(showListOfFilms(films));
+
+//1.3Task
+function setFilmsIds(arr) {
+  return arr.map((film, i) => {
+    film.id = i;
+    return film;
+  });
+}
+
+//1.4Task
+const tranformedArray = setFilmsIds(films);
+
+function checkFilms(arr) {
+  return arr.every((film) => film.id || film.id === 0);
+}
+// console.log(checkFilms(tranformedArray));
+// checkFilms(tranformedArray);
+
+//2.1Task
+const funds = [
+  { amount: -1400 },
+  { amount: 2400 },
+  { amount: -1000 },
+  { amount: 500 },
+  { amount: 10400 },
+  { amount: -11400 },
+];
+
+const getPositiveIncomeAmount = (data) => {
+  return data
+    .filter((item) => item.amount > 0)
+    .reduce((sum, item) => sum + item.amount, 0);
+};
+// console.log(getPositiveIncomeAmount(funds));
+
+//2.2Task
+const getTotalIncomeAmount = (data) => {
+  return data.reduce(
+    (sum, item) =>
+      data.some((curr) => curr.amount < 0)
+        ? sum + item.amount
+        : getPositiveIncomeAmount(data),
+    0
+  );
+};
+console.log(getTotalIncomeAmount(funds));
