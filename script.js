93e3ffdfe34864fde3b95441fd68fac70a7fb244
Atakhan <example@gmail.com>
2024-10-01 18:53:18 +0600

    Инкапсуляция

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 "use strict";
 
-const person = {
-  name: "Taha",
-  age: 28,
+class User {
+  constructor(name, age) {
+    this.name = name;
+    this._age = age;
+  }
 
-  get userAge() {
-    return this.age;
-  },
+  #surname = "Swon";
 
-  set userAge(num) {
-    this.age = num;
-  },
-};
-console.log((person.userAge = 30));
-console.log(person.userAge);
+  say = () => {
+    console.log(
+      `Меня зовут ${this.name} ${this.#surname}, мой возраст: ${this._age}`
+    );
+  };
+
+  get age() {
+    return this._age;
+  }
+
+  set age(num) {
+    if (typeof num === "number" && num > 0 && num < 110) {
+      this._age = num;
+    } else {
+      console.log("Incorrect");
+    }
+  }
+}
+
+const ivan = new User("Ivan", 27);
+// console.log(ivan.age);
+// ivan.age = 99;
+console.log(ivan.surname);
+
+ivan.say();
