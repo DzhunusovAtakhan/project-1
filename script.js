bcf8072d2693b730922f01effd1be99d775d6289
Atakhan <example@gmail.com>
2024-10-03 18:14:01 +0600

    Создание своих ошибок

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 "use strict";
 
-// const number = 1;
+const data = [
+  {
+    id: "box",
+    tag: "div",
+  },
+  {
+    id: "",
+    tag: "nav",
+  },
+  {
+    id: "circle",
+    tag: "span",
+  },
+];
 
-// (function () {
-//   let number = 2;
-//   console.log(number);
-//   console.log(number + 3);
-// })();
+try {
+  data.forEach((blockObj, i) => {
+    const block = document.createElement(blockObj.tag);
 
-// console.log(number);
+    if (!blockObj.id)
+      throw new SyntaxError(`В данных под номером ${i + 1} нет id`);
 
-const user = (function () {
-  const privat = function () {
-    console.log("I am privat");
-  };
-  return {
-    sayHello: privat,
-  };
-})();
+    block.setAttribute("id", blockObj.id);
+    document.body.append(block);
+  });
+} catch (e) {
+  if (e.name === "SyntaxError") {
+    console.log(e.name);
+  } else throw e;
+}
 
-user.sayHello();
+const err = new SyntaxError("fjksfksf");
+console.log(err.name, err.message, err.stack);
